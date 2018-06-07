ce40a1976b593dc708afa6ad8978cd5c31308d67
RenukaT <39990374+RenukaT@users.noreply.github.com>
2018-06-07 15:52:15 +0100

    committed

--- renu_java/src/Arrays/GitClass.java
+++ renu_java/src/Arrays/GitClass.java
@@ -5,7 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git Class");
-		
+		System.out.println("Git Staging");
 	}
 
 }
