b8fbabacfda55b1043e593c0950d5d6dc8358f1c
RenukaT <39990374+RenukaT@users.noreply.github.com>
2018-08-20 21:29:11 +0100

    file added

---
+++ Java_renuka/src/Renu_java.java
@@ -0,0 +1,13 @@
+public class Renu_java.java {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+        int x=10;
+		int y=20;
+		int c=x+y;
+		System.out.println(x+y);
+		
+	}
+   
+}
